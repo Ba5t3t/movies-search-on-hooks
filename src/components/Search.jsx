1f3ffd092fb5015ec641d0023618477ab02001e5
Анна Манзилевская <Ba5t3t>
2023-08-19 16:51:04 +0300

    added refactored movies project

---
+++ src/components/Search.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+function Search(props) {
+  const { searchMovies = Function.prototype } = props;
+
+  const [search, setSearch] = useState("");
+  const [type, setType] = useState("all");
+
+  const handleKey = (e) => {
+    if (e.key === "Enter" && search) {
+      searchMovies(search, type);
+    }
+  };
+
+  const handleFilter = (e) => {
+    setType(e.target.dataset.type);
+    searchMovies(search, e.target.dataset.type);
+  };
+
+  return (
+    <div className='row'>
+      <div className='col s12'>
+        <div className='input-field'>
+          <input
+            className='validate'
+            placeholder='Search'
+            type='search'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={handleKey}
+          />
+          <button
+            className='btn search-btn'
+            onClick={() => {
+              searchMovies(search, type);
+            }}
+          >
+            Search
+          </button>
+          <div>
+            <label>
+              <input
+                className='with-gap'
+                name='type'
+                type='radio'
+                data-type='all'
+                onChange={handleFilter}
+                checked={type === "all"}
+              />
+              <span>All</span>
+            </label>
+            <label>
+              <input
+                className='with-gap'
+                name='type'
+                type='radio'
+                data-type='movie'
+                onChange={handleFilter}
+                checked={type === "movie"}
+              />
+              <span>Movies</span>
+            </label>
+            <label>
+              <input
+                className='with-gap'
+                name='type'
+                type='radio'
+                data-type='series'
+                onChange={handleFilter}
+                checked={type === "series"}
+              />
+              <span>Series</span>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { Search };
---
+++ src/layout/Main.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function Main() {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const searchMovies = (searchInput, type = "all") => {
+    setLoading(true);
+    fetch(
+      `http://www.omdbapi.com/?apikey=32a9b50f&s=${searchInput}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetch(`http://www.omdbapi.com/?apikey=32a9b50f&s=blue`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className='container content'>
+      <Search searchMovies={searchMovies} />
+      {loading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
+}
+
+export { Main };
